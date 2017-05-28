51bb15aea109a9ad8bd948452d57f962f8c9d194
Roman Vesely <crazko@gmail.com>
2017-05-28 17:20:40 +0200

    added manifest for css hash

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
+var crypto = require('crypto');
 var del = require('del');
+var fs = require('fs');
+
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
@@ -19,13 +22,16 @@ var paths = {
   },
 };
 
-gulp.task('default', gulp.series(clean, gulp.parallel(styles, scripts, generate)));
-gulp.task('build', gulp.series(clean, gulp.parallel(buildStyles, buildScripts, generate)));
-gulp.task('watch', gulp.series(clean, gulp.parallel(styles, scripts, generate, watch)));
+gulp.task('default', gulp.series(clean, gulp.parallel(scripts, styles), manifest, generate));
+gulp.task('build', gulp.series(clean, gulp.parallel(buildScripts, buildStyles), manifest,  generate));
+gulp.task('watch', gulp.series(clean, generate, gulp.parallel(styles, scripts, watch)));
 gulp.task(clean);
 
 function clean() {
-  return del([paths.dist]);
+  return del([
+    paths.dist,
+    'src/_config/manifest.neon'
+  ]);
 };
 
 function styles() {
@@ -73,6 +79,14 @@ function generate() {
 function reload(done) {
   browserSync.reload();
   done();
+}
+
+function manifest(callback) {
+  var css = fs.readFileSync(paths.styles.dist + '/styles.css', 'utf8');
+  var hash = crypto.createHash('md5').update(css).digest('hex').slice(0, 10);
+
+  return fs.writeFile('src/_config/manifest.neon', `manifest:
+	css: ${hash}`, callback);
 }
 
 function watch() {
