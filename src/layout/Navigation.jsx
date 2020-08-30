1d4adad14c3bad04354a5443b351595869eabd90
Roman Vesely <crazko@gmail.com>
2020-08-30 18:25:30 +0200

    Move blogroll to separate page

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+const links = {
+  home: '/',
+  about: '/about',
+  log: '/log',
+  blogroll: '/blogroll',
+};
+
 export const Navigation = () => (
   <nav role="navigation" className="navigation">
     <ul className="nav">
-      <li className="nav__item">
-        <Link to="/" activeClassName="nav__link--active">
-          home
-        </Link>
-      </li>
-      <li className="nav__item">
-        <Link to="/about" activeClassName="nav__link--active">
-          about
-        </Link>
-      </li>
-      <li className="nav__item">
-        <Link to="/log" activeClassName="nav__link--active">
-          log
-        </Link>
-      </li>
+      {Object.entries(links).map(([title, to]) => (
+        <li className="nav__item">
+          <Link to={to} activeClassName="nav__link--active">
+            {title}
+          </Link>
+        </li>
+      ))}
     </ul>
   </nav>
 );
