d695f52ee165fb76852402fb0af86090528e8c8e
Roman Vesely <crazko@gmail.com>
2022-12-17 22:16:04 +0100

    update mastodon account, see if rel=me will work

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -37,7 +37,7 @@ export const Header = () => (
                 <i className="icon-linkedin"></i>
                 <span className="sr-only">linkedin</span>
               </a>
-              <a href="https://mastodon.social/@romanvesely" title="mastodon" rel="me">
+              <a href="https://romanvesely.com/@roman" title="mastodon" rel="me">
                 <i className="icon-mastodon"></i>
                 <span className="sr-only">mastodon</span>
               </a>
