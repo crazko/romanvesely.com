20e4c50c6bdb84fcc249a0d541619f0b768519e6
Roman Vesely <crazko@gmail.com>
2024-12-26 12:29:02 +0100

    Update icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import react from '@astrojs/react';
 import rehypeFigure from 'rehype-figure';
 
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 import { data } from './src/data';
 import { remarkReadingTime, remarkGithubEdit, remarkAddSignature } from './src/utils/remark.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), icon()],
 
   markdown: {
     remarkPlugins: [remarkReadingTime, remarkGithubEdit, remarkAddSignature],
